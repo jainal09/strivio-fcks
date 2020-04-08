56075b9b214805a22ef6cec5c386ec54144b1529
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-04-09 03:39:47 +0530

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Program
+{
+  public static void main(String args[])
+  {
+     int num;
+   
+     Scanner in = new Scanner(System.in);
+     num = in.nextInt();
+     System.out.println(num+1);
+  }
+}
